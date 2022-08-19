3d3426807a438abfc918ce831bc1dc45076ae7cf
Victor Araujo <victor_emanuel9@hotmail.com>
2022-08-19 19:45:16 -0300

    change: remove header from Home

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import Header from '../../Components/Header';
 import MainContent from '../../Components/MainContent';
 import NewsTab from '../../Components/NewsTab';
 
 export default function Home() {
   return (
     <>
-      <Header />
       <MainContent>
         <NewsTab />
       </MainContent>
