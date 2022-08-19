712f9eee3c8c8a91884c435494fbbb68344dfa8b
Victor Araujo <victor_emanuel9@hotmail.com>
2022-08-19 18:43:00 -0300

    change(Router): added header to load to every page

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Home from "../Pages/Home"
-import NotFound from "../Pages/NotFound"
+import Home from "../Pages/Home";
+import NotFound from "../Pages/NotFound";
+import Header from "../Components/Header";
 
 export default function appRoute() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route exact path="/" element={<Navigate to="/home" />} />
         <Route exact path="/home" element={<Home />} />
