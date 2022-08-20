843922a89d244a39c005678067ee97785107277d
Victor Araujo <victor_emanuel9@hotmail.com>
2022-08-20 14:27:54 -0300

    feat: change title automatically on home component

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import MainContent from '../../Components/MainContent';
 import NewsTab from '../../Components/NewsTab';
 
 export default function Home() {
+
+  const modifytitle = () => {
+    const newTitle = `${document.title} - Home`
+    document.title = newTitle;
+  }
+
+  useEffect(() => {
+    modifytitle();
+  });
+
   return (
     <>
       <MainContent>
