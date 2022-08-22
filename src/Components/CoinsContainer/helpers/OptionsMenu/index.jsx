6db687d261f332047b266b1b385ae5f9107c67a7
Victor Araujo <victor_emanuel9@hotmail.com>
2022-08-22 15:55:46 -0300

    feat: items per page selector

--- src/Components/CoinsContainer/helpers/OptionsMenu/index.jsx
+++ src/Components/CoinsContainer/helpers/OptionsMenu/index.jsx
@@ -13,6 +13,10 @@ export default function OptionsMenu(props) {
     setOrderBy(target.value);
   }
 
+  const onchangeItemsPerPage = ({ target }) => {
+    setItemsQuantity(target.value);
+  }
+
   return (
     <section>
         <label htmlFor="coinsContainer-order-by">
@@ -22,6 +26,16 @@ export default function OptionsMenu(props) {
             <option value="market_cap_desc">Market Cap Descending</option>
             <option value="volume_asc">Volume Ascending</option>
             <option value="volume_desc">Volume Descending</option>
+          </select>
+        </label>
+
+        <label htmlFor="coinsContainer-items-by-page">
+          Assets per Page:
+          <select id="coinsContainer-items-by-page" onChange={onchangeItemsPerPage}>
+            <option value="10">10</option>
+            <option value="25">25</option>
+            <option value="35">35</option>
+            <option value="50">50</option>
           </select>
         </label>
       </section>
