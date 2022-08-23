83cba9f60f3ab69e24d218ad083f40fbebf3ba14
Victor Araujo <victor_emanuel9@hotmail.com>
2022-08-22 21:12:50 -0300

    change: electron window size

--- electron/main.js
+++ electron/main.js
@@ -3,15 +3,15 @@ const path = require('path')
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1280,
+    height: 720,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
 
   win.loadURL('http://localhost:3000/')
-  win.webContents.openDevTools()
+  // win.webContents.openDevTools()
 }
 
 app.whenReady().then(() => {
